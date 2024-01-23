0fb7bacf2444b2a8897d0cc03946a5f6eca36bfd
ryochinzoo <h.ryohei.titech@gmail.com>
2024-01-23 23:18:02 +0100

    strapi backup

---
+++ backend/config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+    //...
+    'import-export-entries': {
+      enabled: true,
+      config: {
+        // See `Config` section.
+        
+        serverPublicHostname: 'https://thecompanyberlin.com',
+      },
+    },
+    //...
+  })
